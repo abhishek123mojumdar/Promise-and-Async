10ce369d05b78eb249888446d546e8eb3c11fe84
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2022-06-21 23:15:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ab14ux
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ab14ux)
---
+++ index.js
@@ -0,0 +1,33 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Promises and Async</h1>`;
+document.getElementById('btn').addEventListener('click', myFunction);
+
+function myFunction() {
+  var html = ``;
+  fetch('https://api.covid19api.com/summary')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let countries = data.Countries;
+      countries.forEach((country, index) => {
+        if (index < 5) {
+          html =
+            html +
+            `<div class="card">
+        <div class="card-content">
+        <h4 style="text-align:center">${country.Country}<h4>
+        <hr>
+        <p> Total confirmed : ${country.TotalConfirmed}</p>
+        <p> Total deaths : ${country.TotalDeaths}</p>
+        </div>
+        </div>`;
+        }
+      });
+      console.log(html);
+      document.getElementById('cardEle').innerHTML = html;
+    });
+}
